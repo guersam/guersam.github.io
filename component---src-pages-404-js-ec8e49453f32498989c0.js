aa0654d39d625a345b7f5ccaea1963d969a80608
guersam <guersam@users.noreply.github.com>
2022-11-08 14:03:10 +0000

    deployed via Gatsby Publish Action 🎩 for 1048f62d51a982189d720fefd73f2462ec6049cd

---
+++ component---src-pages-404-js-ec8e49453f32498989c0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkguersam_com=self.webpackChunkguersam_com||[]).push([[883],{429:function(t,e,a){a.r(e);var n=a(7294),u=a(8678),l=a(262);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(u.Z,{location:a,title:o},n.createElement(l.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ec8e49453f32498989c0.js.map
